e757339fae4d9e08adf3324d3178452297c09a9f
Sergey S. Betke <sergey.s.betke@yandex.ru>
2019-08-18 01:01:38 +0300

    test: Refactor Wait-ExpectedMessage tests

--- tests/public/Wait-ExpectedMessage.Tests.ps1
+++ tests/public/Wait-ExpectedMessage.Tests.ps1
@@ -2,26 +2,25 @@
 
 Describe 'Wait-ExpectedMessage' {
 
-    [String] $TestConsoleOutputContent1 = @'
+    BeforeEach {
+        [String] $TestConsoleOutputContent1 = @'
 test result
 [admin@router] >
 
 '@;
+        $Reader = New-Object System.IO.StringReader( $TestConsoleOutputContent1 );
+    }
 
     Context "When not passed prompt template." {
-
-        $Reader = New-Object System.IO.StringReader( $TestConsoleOutputContent1 );
         It 'Should return correct result.' {
             Wait-ITGSerialTerminalExpectedMessage `
                 -ConsoleStreamReader $Reader `
                 -PassThru `
             | Should Be "test result`r`n"
         }
-
     }
-    Context "When passed correct prompt template." {
 
-        $Reader = New-Object System.IO.StringReader( $TestConsoleOutputContent1 );
+    Context "When passed correct prompt template." {
         It 'Should return correct result.' {
             Wait-ITGSerialTerminalExpectedMessage `
                 -ConsoleStreamReader $Reader `
@@ -29,11 +28,9 @@ test result
                 -PassThru `
             | Should Be "test result`r`n"
         }
-
     }
-    Context "When console output does not contains prompt template." {
 
-        $Reader = New-Object System.IO.StringReader( $TestConsoleOutputContent1 );
+    Context "When console output does not contains prompt template." {
         It 'Should throw timeout error.' {
             {
                 Wait-ITGSerialTerminalExpectedMessage `
@@ -43,7 +40,6 @@ test result
                     -PassThru `
             } | Should -Throw
         }
-
     }
 
 }
