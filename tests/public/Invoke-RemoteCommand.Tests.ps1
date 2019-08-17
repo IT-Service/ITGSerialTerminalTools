70104c7989f6c0a60292b28020c0ee35300f8e89
Sergey S. Betke <sergey.s.betke@yandex.ru>
2019-08-18 01:07:33 +0300

    test: Add some tests for Invoke-RemoteCommand

--- tests/public/Invoke-RemoteCommand.Tests.ps1
+++ tests/public/Invoke-RemoteCommand.Tests.ps1
@@ -38,27 +38,32 @@ ${Prompt}
             }
         }
 
-        # Context "When passed correct prompt template." {
-        #     It 'Should return correct result.' {
-        #         Invoke-ITGSerialTerminalRemoteCommand `
-        #             -ConsoleStreamReader $Reader `
-        #             -PromptPattern '\[.+?\] >' `
-        #             -PassThru `
-        #         | Should Be "test result`r`n"
-        #     }
-        # }
+        Context "When console output does not contains prompt template." {
+            BeforeEach {
+                [String] $Command = 'test command';
+                [String] $Prompt = '[admin@router] >';
+                [String] $TestConsoleOutputContent1 = @"
+${Prompt} ${Command}
+${Prompt} ${Command}
+test result
+${Prompt}
 
-        # Context "When console output does not contains prompt template." {
-        #     It 'Should throw timeout error.' {
-        #         {
-        #             Invoke-ITGSerialTerminalRemoteCommand `
-        #                 -ConsoleStreamReader $Reader `
-        #                 -PromptPattern '\[notadmin\] >' `
-        #                 -Timeout ( New-Object System.TimeSpan( 0, 0, 1 ) ) `
-        #                 -PassThru `
-        #         } | Should -Throw
-        #     }
-        # }
+"@;
+                $Reader = New-Object System.IO.StringReader( $TestConsoleOutputContent1 );
+                $Writer = New-Object System.IO.StringWriter;
+            }
+            It 'Should throw timeout error.' {
+                {
+                    Invoke-ITGSerialTerminalRemoteCommand `
+                        -ConsoleStreamReader $Reader `
+                        -ConsoleStreamWriter $Writer `
+                        -Command $Command `
+                        -PromptPattern '\[unknownPrompt\] >' `
+                        -Timeout ( New-Object System.TimeSpan( 0, 0, 1 ) ) `
+                        -PassThru `
+                } | Should -Throw
+            }
+        }
 
     }
 
